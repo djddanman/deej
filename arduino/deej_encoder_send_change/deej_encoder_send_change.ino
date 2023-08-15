11602c1b011d886adf90b820ca8c361477b607cb
djddanman <31169855+djddanman@users.noreply.github.com>
2023-08-15 18:52:41 -0500

    Add files via upload

--- arduino/deej_encoder_send_change/deej_encoder_send_change.ino
+++ arduino/deej_encoder_send_change/deej_encoder_send_change.ino
@@ -1,34 +1,53 @@
 #include "pio_encoder.h"
 #include "Adafruit_TinyUSB.h"
 
-const int NUM_SLIDERS = 5;
-PioEncoder encoders[NUM_SLIDERS] = {0, 2, 4, 6, 8};
+const int NUM_SLIDERS = 5; // Change to 5 if uncommenting last encoder in array
 
+PioEncoder encoders[NUM_SLIDERS] = {
+    PioEncoder(0, pio0),
+    PioEncoder(2, pio0),
+    PioEncoder(4, pio0),
+    PioEncoder(6, pio0),
+    PioEncoder(8, pio1) // Works every time if this line is commented
+};
 
 int EncoderValues[NUM_SLIDERS];
 
-void setup() {
+void setup() { 
+  
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
   for (int i = 0; i < NUM_SLIDERS; i++) {
     encoders[i].begin();
     EncoderValues[i] = 0;
+    for (int j = 0; j <= i; j++) {
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(100);
+      digitalWrite(LED_BUILTIN, LOW);
+      delay(100);
+    }
+    delay(500);
   }
-  //EncoderValues[0] = 50;
-
+  
   Serial.begin(9600);
   while(!Serial);
+  Serial.write("Finishing init\n");
 }
 
 void loop() {
   updateSliderValues();
   sendSliderValues(); // Actually send data (all the time)
-  // printSliderValues(); // For debug
+  // printSliderValues(); // For debug, comment out for normal use
   delay(10);
 }
 
 void updateSliderValues() {
   for (int i = 0; i < NUM_SLIDERS; i++) {
     int count = encoders[i].getCount();
-      EncoderValues[i] = count;
+    EncoderValues[i] = count;
     encoders[i].reset();
   }
 }
